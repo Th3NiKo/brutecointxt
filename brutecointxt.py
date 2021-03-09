33835123084f8ada52ab6ca3fd2fcff8e518ecfc
Nikodem Pachala <niko.pachala@onet.eu>
2021-03-09 13:29:09 +0100

    Update brutecointxt.py

--- brutecointxt.py
+++ brutecointxt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Simple script to brute force text file filled with passphrases
+Simple script to brute force bitcoin wallets with text file filled with passphrases
 and print any keys with available balance. Program check for
 both compressed and uncompressed versions of addresses using
 bit library.
